5ba9304432cca2de6e715081b0f3a6ef87a03c2c
GagarinSN <gagarin.sn@yandex.ru>
2023-09-09 21:19:03 +0500

    Init commit

---
+++ src/main/java/ru/practikum/teamonesolution/TeamOneSolutionApplication.java
@@ -0,0 +1,13 @@
+package ru.practikum.teamonesolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamOneSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamOneSolutionApplication.class, args);
+	}
+
+}
