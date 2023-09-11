43ea95457727c85f0d786449993d329382d25858
AleksandrLiagushin <a.lyagushin@yandex.ru>
2023-09-11 23:43:20 +0700

    refactor: brutforce

--- src/main/java/ru/practikum/teamonesolution/TeamOneSolutionApplication.java
+++ src/main/java/ru/practikum/teamonesolution/TeamOneSolutionApplication.java
@@ -1,40 +1,52 @@
 package ru.practikum.teamonesolution;
 
+import com.google.gson.Gson;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import ru.practikum.teamonesolution.client.ProgrammerDayClient;
+import ru.practikum.teamonesolution.models.BadResponse;
 import ru.practikum.teamonesolution.models.Password;
 import ru.practikum.teamonesolution.service.Decoder;
 
 @SpringBootApplication
 public class TeamOneSolutionApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        char[] chars = new char[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f'};
+        System.out.println(generateCombinations("", "0123456789ABCDEFabcdef", 8));
+
+    }
 
 
-		System.out.println(generateCombinations("", "0123456789ABCDEFabcdef", 8));
+    public static String generateCombinations(String prefix, String characters, int remainingLength) {
+        ProgrammerDayClient programmerDayClient = new ProgrammerDayClient();
+        Password password = new Password();
+        int[] pass = new int[]{11, 11, 11, 11, 10, 10, 10, 10};
+        StringBuilder builder = new StringBuilder();
+        int status = 0;
+        int sign = 7;
 
-	}
+        while (status != 200) {
 
-	public static String generateCombinations(String prefix, String characters, int remainingLength) {
-		ProgrammerDayClient programmerDayClient = new ProgrammerDayClient();
-		Password password = new Password();
-		if (remainingLength == 0) {
-			System.out.println(prefix);
-			return prefix;
-		}
+            for (int i : pass) {
+                builder.append(pass[i]);
+            }
 
-		for (int i = 0; i < characters.length(); i++) {
-			String newPrefix = prefix + characters.charAt(i);
-			String dataToSend = "{\"password\": \"" + newPrefix + "\"}";
-			programmerDayClient.getTask(dataToSend);
-			if (password.getStatus() == 200) {
-				return password.getJson();
-			};
+            String dataToSend = "{\"password\": \"" + builder.toString() + "\"}";
 
-			generateCombinations(newPrefix, characters, remainingLength - 1);
-		}
-		return prefix;
-	}
+            Password newPass = programmerDayClient.getTask(dataToSend);
+            status = newPass.getStatus();
 
-}
+
+        }
+
+    }
+
+    private static boolean isPromptGreaterThan(String json) {
+        Gson gson = new Gson();
+        BadResponse response = gson.fromJson(json, BadResponse.class);
+
+        String prompt = response.getPromt();
+
+        return ">pass".equals(prompt);
+    }
